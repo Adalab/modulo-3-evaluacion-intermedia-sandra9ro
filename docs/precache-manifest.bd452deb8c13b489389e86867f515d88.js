ff8b29e63b4c006a49a8d64b29d022aecc66c85f
Sandra Romero <romeroortiz.sandra@gmail.com>
2019-12-20 08:37:04 +0100

    Initial commit

---
+++ docs/precache-manifest.bd452deb8c13b489389e86867f515d88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2eb15406b91b9ff0049da4e760296f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c382140ec8f02195df1a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9cb8135c6a8b9ba96343",
+    "url": "/static/js/2.3bef25da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3bef25da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c382140ec8f02195df1a",
+    "url": "/static/js/main.58998fce.chunk.js"
+  },
+  {
+    "revision": "384f0e7ba7e2ae4b74e5",
+    "url": "/static/js/runtime-main.2b58dd57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
